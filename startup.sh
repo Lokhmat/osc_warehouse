05444e48468d909498317405b834a1a4bbe146bc
Sergey Lokhmatikov <lohmat@yandex-team.ru>
2024-05-13 22:16:51 +0300

    F

--- startup.sh
+++ startup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-docker compose up --build --detach
+docker compose up --build --detach;
 
-crontab -r
-echo "0 0 1,20 * * rm postgres_dump.gzip; sudo docker exec -t database pg_dumpall -c | gzip > ./postgres_dump.gzip" >> mycron
-crontab mycron
-rm mycron
+crontab -r;
+echo "0 0 1,20 * * rm postgres_dump.gzip; sudo docker exec -t database pg_dumpall -c | gzip > ./postgres_dump.gzip" >> mycron;
+crontab mycron;
+rm mycron;
 
-docker exec -it database /bin/bash -c ".venv/bin/python3 -m pgmigrate -c '' migrate -t latest"
+docker exec -it database /bin/bash -c ".venv/bin/python3 -m pgmigrate -c '' migrate -t latest";
