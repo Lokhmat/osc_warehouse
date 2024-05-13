4d773844d53dd576049ca40361810677e09d6525
Sergey Lokhmatikov <lohmat@yandex-team.ru>
2024-05-13 22:00:57 +0300

    Try to add cron

--- startup.sh
+++ startup.sh
@@ -2,4 +2,11 @@
 
 docker compose up --build --detach
 
+crontab -r
+
+crontab -l > mycron
+echo "1 * * * * sudo docker exec -t database pg_dumpall -c > dump_$(date +%Y-%m-%d_%H_%M_%S).sql" >> mycron
+crontab mycron
+rm mycron
+
 docker exec -it database /bin/bash -c ".venv/bin/python3 -m pgmigrate -c '' migrate -t latest"
